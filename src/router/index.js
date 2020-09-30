9fab153a2097de2a803a184f1168732cece3178a
Basruhe <sb.ruhe@gmail.com>
2020-09-30 16:00:16 +0200

    dynamic importing/lazy loading for links

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,11 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
-import Brazil from "../views/Brazil.vue";
-import Hawaii from "../views/Hawaii.vue";
-import Jamaica from "../views/Jamaica.vue";
 import Panama from "../views/Panama.vue";
 
 Vue.use(VueRouter);
+
+// Exercise note:: the 'Home' and 'Panama" pages load all js from the start. The rest of the pages 'lazy load' the js only when the user actually navigates to the path.
 
 const routes = [
   {
@@ -20,23 +19,22 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    component: () => import("../views/About.vue"),
   },
   {
     path: "/brazil",
     name: "brazil",
-    component: Brazil,
+    component: () => import("../views/Brazil.vue"),
   },
   {
     path: "/hawaii",
     name: "hawaii",
-    component: Hawaii,
+    component: () => import("../views/Hawaii.vue"),
   },
   {
     path: "/jamaica",
     name: "jamaica",
-    component: Jamaica,
+    component: () => import("../views/Jamaica.vue"),
   },
   {
     path: "/panama",
