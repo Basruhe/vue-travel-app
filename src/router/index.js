219cbb07d01b5f08a3869e53b96155ba9cb7cad4
Basruhe <sb.ruhe@gmail.com>
2020-09-30 16:07:01 +0200

    added webpackchunknames for routes

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,7 @@ import Panama from "../views/Panama.vue";
 Vue.use(VueRouter);
 
 // Exercise note:: the 'Home' and 'Panama" pages load all js from the start. The rest of the pages 'lazy load' the js only when the user actually navigates to the path.
+// Exercise note: most paths received webpackchunknames. This way the js files are named, and they show up under these names when you inspect (useful for debugging?)
 
 const routes = [
   {
@@ -19,22 +20,26 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import("../views/About.vue"),
+    component: () =>
+      import(/* WebPackChunkName="about" */ "../views/About.vue"),
   },
   {
     path: "/brazil",
     name: "brazil",
-    component: () => import("../views/Brazil.vue"),
+    component: () =>
+      import(/* WebPackChunkName="brazil" */ "../views/Brazil.vue"),
   },
   {
     path: "/hawaii",
     name: "hawaii",
-    component: () => import("../views/Hawaii.vue"),
+    component: () =>
+      import(/* WebPackChunkName="hawaii"*/ "../views/Hawaii.vue"),
   },
   {
     path: "/jamaica",
     name: "jamaica",
-    component: () => import("../views/Jamaica.vue"),
+    component: () =>
+      import(/* WebPackChunkName="jamaica"*/ "../views/Jamaica.vue"),
   },
   {
     path: "/panama",
